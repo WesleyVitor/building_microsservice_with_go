a7f5673487a0701410277d4d6f4771110001bac5
WesleyVitor <wesleydemorais@outlook.com.br>
2022-07-09 13:04:16 -0300

    feat: Gracefull shutdown

--- working/main.go
+++ working/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/WesleyVitor/handlers"
 )
@@ -15,11 +19,39 @@ func main(){
 	helloHandler := handlers.NewHello(l)
 	goodbyeHandler := handlers.NewGoodBye(l)
 
-	server := http.NewServeMux()
-	server.Handle("/",helloHandler)
-	server.Handle("/goodbye", goodbyeHandler)
+	routes := http.NewServeMux()
+	routes.Handle("/",helloHandler)
+	routes.Handle("/goodbye", goodbyeHandler)
 
+	server := &http.Server{
+		Addr: ":9000",
+		Handler: routes,
+		IdleTimeout: 150 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func(){
+		err := server.ListenAndServe()
+		if err != nil{
+			l.Println(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal,1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, syscall.SIGTERM)
 	
-	http.ListenAndServe(":9000", server)
+	sig :=<- sigChan
+	l.Printf("\nReceive terminated, graceful shutdown %s\n",sig)
+	ctx,cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer func ()  {
+		l.Printf("Terminar db")
+		cancel()
+	}()
+	server.Shutdown(ctx)
+
+
+
 
 }
