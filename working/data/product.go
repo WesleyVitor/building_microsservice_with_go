986bae4699f2cd3cbce66dff8e324f210beb2a7e
WesleyVitor <wesleydemorais@outlook.com.br>
2024-12-29 14:58:26 -0300

    feat: ep4

--- working/data/product.go
+++ working/data/product.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Product struct {
 	ID          int     `json:"id"`
@@ -13,10 +16,44 @@ type Product struct {
 	DeletedOn   string  `json:"-"`
 }
 
+var ErrProductNotFound = errors.New("Product not found")
 
+func UpdateProduct(id int, p *Product) error{
+	_, pos := findProduct(id)
+	if pos == -1 {
+		return ErrProductNotFound
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+func findProduct(id int) (*Product, int) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i
+		}
+	}
+	return nil, -1
+}
 
 func GetProducts() []*Product {
 	return productList
+}
+
+func AddProduct(p *Product) {
+
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func getNextID() int {
+	if len(productList) == 0 {
+		return 1
+	}
+	lp := productList[len(productList)-1]
+	
+	return lp.ID + 1
 }
 
 var productList = []*Product{
--- working/handlers/products.go
+++ working/handlers/products.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/WesleyVitor/data"
 )
@@ -21,6 +23,16 @@ func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(w, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		log.Println("Update a product")
+		p.updateProduct(w, r)
+	}
+
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -33,4 +45,41 @@ func (p *Products) getProducts(w http.ResponseWriter, _ *http.Request) {
 	if err != nil {
 		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
+	product := &data.Product{}
+	
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(product)
+	if err != nil {
+		log.Println("Error deserializing product", err)
+		http.Error(w, "Unable to unmarshal json", http.StatusBadRequest)
+		return
+	}
+
+	data.AddProduct(product)
+}
+
+func (p *Products) updateProduct(w http.ResponseWriter, r *http.Request) {
+	log.Println("Update a product", r.URL.Query())
+	id := r.URL.Query().Get("id")
+
+	product := &data.Product{}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(product)
+	if err != nil {
+		log.Println("Error deserializing product", err)
+		http.Error(w, "Unable to unmarshal json", http.StatusBadRequest)
+		return
+	}
+
+	id_int, _ := strconv.Atoi(id)
+	err = data.UpdateProduct(id_int, product)
+	if err == data.ErrProductNotFound {
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	}
+
 }
